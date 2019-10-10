7b7d1beeaee25bc0065ef8b19afc6954c163eff7
antznin <antonin.godard@telecom-paristech.fr>
2019-10-10 11:59:17 -0400

    Add functions

--- ctrlauto.cpp
+++ ctrlauto.cpp
@@ -4,6 +4,7 @@
 
 #include <time.h>
 #include <math.h>
+#include <string.h>
 
 #define PI 3.141592
 
@@ -125,11 +126,13 @@ double getClosestStation(coord_t actualPos, coord_t &stationPos)
 
 struct TaskData 
 {
-    coord_t *stationPos;
-    coord_t *dest;
-    coord_t *currPos;
-    coord_t *nextStep;
-    bool     needToCharge;
+    coord_t stationPos;
+    coord_t dest;
+    coord_t currPos;
+    coord_t nextStep;
+    coord_t lastCamPos;
+    bool    needToCharge;
+    bool    beginCycle;
 };
 
 double alerte10_code(int seg, void * m_data) {
@@ -153,21 +156,51 @@ double alerte80_code(int seg, void * m_data) {
      default:
          return FINISHED;
     }
-            
 }
 
 double getAngle(coord_t currPos, coord_t nextPos) {
-      return (atan((nextPos.x - currPos.x)/(nextPos.y - currPos.y)) * (180/PI));
+    if (nextPos.y - currPos.y > 0) {
+        return (atan((nextPos.x - currPos.x)/(nextPos.y - currPos.y)) * (180/PI));
+    } else if (nextPos.y - currPos.y < 0) {
+        return (atan((nextPos.x - currPos.x)/(nextPos.y - currPos.y)) * (180/PI) + 180);
+    } else {
+        if (nextPos.x - currPos.x > 0) {
+            return 90;
+        } else {
+            return -90;
+        }
+    }
 }
 
 double dest_code(int seg, void * data) {
     
-    float deltaX, deltaY;
     TaskData * d = (TaskData *)data;
+    double t;
 
     switch (seg) {
         case 1:
+            ttWait("gendest");
+            t = 0;
+            d->currPos.x = ttAnalogIn(5); d->currPos.y = ttAnalogIn(6);
+            int32_t stepX;
+            int32_t stepY;
+            if (d->needToCharge) {
+                t += getClosestStation(d->currPos, d->stationPos);
+                d->nextStep.x = d->stationPos.x;
+                d->nextStep.y = d->stationPos.y;
+            } else {
+                t += gen_dest(d->currPos, d->dest);
+                t += getNextStep((int)d->dest.x, (int)d->dest.y,
+                                (int)d->currPos.x, (int)d->currPos.y,
+                                stepX, stepY);
+                d->nextStep.x = (double)stepX;
+                d->nextStep.y = (double)stepY;
+            }
             
+            d->beginCycle = false;
+            return (t + 6) * T_INSTRUCTION + 2 * T_READ;
+        default:
+            ttSetNextSegment(1);
     }
 }
 
@@ -175,6 +208,7 @@ double nav_code(int seg , void* data) {
 
     TaskData* d = (TaskData*) data;
     double t;
+    float deltaX, deltaY;
 
     switch(seg) {
         case 1:
@@ -183,29 +217,15 @@ double nav_code(int seg , void* data) {
             } else {
                 ttAnalogOut(4, 50);
             }
-            d->currPos->x = ttAnalogIn(5); // recup de la pos
-            d->currPos->y = ttAnalogIn(6);
-            return 2 * T_READ + 3 * T_INSTRUCTION;
+            return 1 * T_INSTRUCTION;
         case 2:
-            // calcul de la prochaine étape
-            int32_t stepX;
-            int32_t stepY;
-            if (d->needToCharge) {
-                t = getClosestStation(*d->currPos, *d->stationPos);
-                t += getNextStep((int)d->stationPos->x, (int)d->stationPos->y,
-                                    (int)d->currPos->x, (int)d->currPos->y,
-                                    stepX, stepY);
-            } else {
-                t = getNextStep((int)d->dest->x, (int)d->dest->y,
-                                (int)d->currPos->x, (int)d->currPos->y,
-                                stepX, stepY);
+            if (d->beginCycle) {
+                ttNotify("gendest");
             }
-            d->nextStep->x = stepX;
-            d->nextStep->y = stepY;
-            return 5 * T_INSTRUCTION + t;
+            return 2 * T_INSTRUCTION;
         case 3:
             // écriture de l'angle
-            ttAnalogOut(3, getAngle(*d->currPos, *d->nextStep));
+            ttAnalogOut(3, getAngle(d->currPos, d->nextStep));
             return T_INSTRUCTION;
         default:
             return FINISHED;
@@ -214,15 +234,13 @@ double nav_code(int seg , void* data) {
 
 double camera_code(int seg, void * data) {
 
-    coord_t lastCamPos;
     double delta;
     TaskData* d = (TaskData*) data;
 
     switch (seg) {
         case 1:
-            lastCamPos.x = ttAnalogIn(5);
-            lastCamPos.y = ttAnalogIn(6);
-            delta = 0;
+            d->lastCamPos.x = ttAnalogIn(5);
+            d->lastCamPos.y = ttAnalogIn(6);
             ttAnalogOut(2, 1);
             return 3 * T_INSTRUCTION;
         case 2:
@@ -232,15 +250,17 @@ double camera_code(int seg, void * data) {
             t_compt = 0;
             while (done == 0) {
                 done = ttAnalogIn(4);
-                t_compt += 2;
+                t_compt += 1;
             }
-            return (t_compt + 2) * T_INSTRUCTION;
+            ttAnalogOut(2, 0);
+            return (t_compt + 3) * T_INSTRUCTION + t_compt * T_READ;
         case 3:
             double t_compt2;
             t_compt2 = 0;
+            delta = 0;
             while (delta < 10) {
-                delta = sqrt(pow((ttAnalogIn(5) - lastCamPos.x), 2) 
-                        + pow((ttAnalogIn(6) - lastCamPos.y), 2));
+                delta = sqrt(pow((ttAnalogIn(5) - d->lastCamPos.x), 2) 
+                           + pow((ttAnalogIn(6) - d->lastCamPos.y), 2));
                         t_compt2 += 2;
             }
             return t_compt2 * T_INSTRUCTION;
@@ -257,19 +277,22 @@ void init()
     ttSetUserData(data);
     
     data->needToCharge = false;
+    data->beginCycle = true;
 
     ttInitKernel(prioFP);
 
+    ttCreateEvent("gendest");
+
     ttCreateHandler("alerte10_handler", 1, alerte10_code, (void *)data);
-    //activate battery save
     ttCreateHandler("alerte80_handler", 1, alerte80_code, (void *)data);
-    //exit charge station
 
     ttAttachTriggerHandler(1, "alerte10_handler");
     ttAttachTriggerHandler(2, "alerte80_handler");
     
     ttCreatePeriodicTask("nav", 0, 0.1, nav_code, (void *)data);
     ttCreatePeriodicTask("camera", 0, 0.1, camera_code, (void *)data);
+    ttCreateJob("camera");
+    ttCreateTask("dest", 0, dest_code, (void *)data);
 
 }
 
