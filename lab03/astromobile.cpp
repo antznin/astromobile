457a2750c56cb172112b1103a402ea01b6be50e3
antznin <antonin.godard@telecom-paristech.fr>
2019-10-29 17:29:07 -0400

    Add init

--- lab03/astromobile.cpp
+++ lab03/astromobile.cpp
@@ -1,9 +1,155 @@
 #include <iostream>
+#include <sched.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <mqueue.h> 
+#include <unistd.h> 
+#include <pthread.h> 
+#include <semaphore.h> 
+#include <sys/types.h> 
+#include <time.h>
+#include <signal.h>
+#include <sys/syspage.h> 
+#include <sys/neutrino.h> 
+
 #include "genMap.h"
+#include "astromobile.h"
+
 using namespace std;
 
-
 int main() {
-	cout << "Hello World!!!" << endl; // prints Hello World!!!
+
+	init();
+
 	return 0;
 }
+
+void * cameraControl_worker(void * data) {
+	return NULL; 
+}	
+void * camera_worker(void * data) {
+	return NULL; 
+}	
+void * battery_worker(void * data) {
+	return NULL; 
+}	
+void * speed_worker(void * data) {
+	return NULL; 
+}	
+void * angle_worker(void * data) {
+	return NULL; 
+}	
+void * navControl_worker(void * data) {
+	return NULL; 
+}	
+void * destControl_worker(void * data) {
+	return NULL; 
+}	
+void * battLow_worker(void * data) {
+	return NULL; 
+}	
+void * battHigh_worker(void * data) {
+	return NULL; 
+}	
+void * display_worker(void * data) {
+	return NULL; 
+}	
+
+void init()
+{
+	pthread_t tid[THREAD_NUM];
+	pthread_attr_t attr;
+	struct sched_param sched; 
+	struct mq_attr mqattr;
+	int i; 
+
+	// création des queues
+	memset(&mqattr, 0, sizeof(mqattr));
+	mqattr.mq_maxmsg = MAX_NUM_MSG;
+	mqattr.mq_msgsize = sizeof(message) ;
+
+	mq_unlink(MQ_CAM) ;
+	if ((msgQCam = mq_open(MQ_CAM, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR,
+			&mqattr)) < 0){
+		printf("msgQCreate MQ_CAM échouée!\n");
+		return;
+	}
+	mq_unlink(MQ_NAV) ;
+	if ((msgQNav = mq_open(MQ_NAV, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR,
+			&mqattr)) < 0){
+		printf("msgQCreate MQ_NAV échouée!\n");
+		return;
+	}
+
+	mq_unlink(MQ_BATT) ;
+	if ((msgQBatt = mq_open(MQ_BATT, O_RDWR|O_CREAT, S_IRUSR|S_IWUSR,
+			&mqattr)) < 0){
+		printf("msgQCreate MQ_BATT échouée!\n");
+		return;
+	}
+
+	// creation des threads
+	setprio(0,20);
+	pthread_attr_init (&attrib);
+	pthread_attr_setinheritsched (&attrib, PTHREAD_EXPLICIT_SCHED);
+	pthread_attr_setschedpolicy (&attrib, SCHED_FIFO);
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[0], &attrib, cameraControl_worker, NULL ) < 0)
+		printf("taskSpawn cameraControl_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[1], &attrib, camera_worker, NULL ) < 0)
+		printf("taskSpawn camera_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[2], &attrib, battery_worker, NULL ) < 0)
+		printf("taskSpawn battery_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[3], &attrib, speed_worker, NULL ) < 0)
+		printf("taskSpawn speed_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[4], &attrib, angle_worker, NULL ) < 0)
+		printf("taskSpawn angle_worker failed!\n");
+	
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[5], &attrib, navControl_worker, NULL ) < 0)
+		printf("taskSpawn navControl_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[6], &attrib, destControl_worker, NULL ) < 0)
+		printf("taskSpawn destControl_worker failed!\n");
+
+	mySchedParam.sched_priority = 1;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[7], &attrib, battLow_worker, NULL ) < 0)
+		printf("taskSpawn battLow_worker failed!\n");
+
+	mySchedParam.sched_priority = 1;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[8], &attrib, battHigh_worker, NULL ) < 0)
+		printf("taskSpawn battHigh_worker failed!\n");
+
+	mySchedParam.sched_priority = 20;
+	pthread_attr_setschedparam(&attrib, &mySchedParam);
+	if ( pthread_create(&tid[9], &attrib, display_worker, NULL ) < 0)
+		printf("taskSpawn display_worker failed!\n");
+
+	// join des threads
+	for (i = 0; i < 10; ++i) {
+		if (pthread_join(tid[i], NULL) < 0)
+			printf("Join failed for thread %d\n", i);
+	}
+}
+
+/* vim: set ts=4 sw=4 tw=90 noet :*/
---
+++ lab03/astromobile.h
@@ -0,0 +1,40 @@
+#ifndef ASTROMOBILE_H
+#define ASTROMOBILE_H
+
+#include "genMap.h"
+
+void init();
+
+#define THREAD_NUM 10
+
+// Maximum de message dans un queue
+#define MAX_NUM_MSG 50
+// Priorités des messages
+#define MSG_PRI_LOW  10
+#define MSG_PRI_MED  20
+#define MSG_PRI_HIGH 30
+
+#define MQ_NAV  "MQ Navigation"
+#define MQ_CAM  "MQ Caméra"
+#define MQ_BATT "MQ Batterie"
+
+struct physicsData {
+	float speed;
+	pic;
+	float battLevel;
+	float angle;
+	coord_t currPos;
+}
+
+typedef enum {MSG_DATA, MSG_ALERTE} msgType;
+
+// structure de message partagé entre les tâches
+typedef struct
+	int ID;
+	msgType type;
+	unsigned int size;
+} message;
+
+mqd_t msgQCam, msgQNav, msgQBatt;
+
+#endif /* ifndef ASTROMOBILE_H */
