06e1e4caac207daa92fa0fb3dec6e1799678e267
Rayan Neggazi <rayan.neggazi@polymtl.ca>
2019-10-30 14:55:13 -0400

    [WIP] Fix threads

--- lab03/astromobile/src/astromobile.cpp
+++ lab03/astromobile/src/astromobile.cpp
@@ -40,14 +40,14 @@ int main() {
 void * cameraControl_worker(void * data) {
     int delta, x, y;
         while(1)        {
-                pthread_mutex_lock(&mut1);
+                pthread_mutex_lock(&mutDataCurrPos);
                 x = myData.currPos.x;
                 y = myData.currPos.y;
-                pthread_mutex_unlock(&mut1);
+                pthread_mutex_unlock(&mutDataCurrPos);
                 while (delta < 10) {
-                        pthread_mutex_lock(&mut1);
+                        pthread_mutex_lock(&mutDataCurrPos);
                         delta = sqrt(pow((myData.currPos.x - x), 2) + pow((myData.currPos.y - y), 2));
-                        pthread_mutex_unlock(&mut1);
+                        pthread_mutex_unlock(&mutDataCurrPos);
                 }
                 takePx = true;
                 delta = 0;
@@ -58,19 +58,33 @@ void * camera_worker(void * data) {
         rgb_t image;
         while(1)        {
                 if (takePx) {
-                        //image = pm.takePhoto(myData.currPos);
                         image = pm.takePhoto(myData.currPos);
+                		pthread_mutex_unlock(&mutDataCurrPos);
                         takePx = false;        }
         }
         return NULL;
 }
 
 void * battery_worker(void * data) {
+
+	float speed_local;
+
+	cout << "battery" << endl;
+
 	while(1)	{
-		if (inCharge)
+		if (inCharge)	{
+			pthread_mutex_lock(&mutDataBattLevel);
 			myData.battLevel += COEFF_CHARGE;
-		myData.battLevel -= COEFF_DECHARGE*myData.speed + CONST_DECHARGE;
-	sleep(1);
+			pthread_mutex_unlock(&mutDataBattLevel);
+		}
+		pthread_mutex_lock(&mutDataBattLevel);
+		pthread_mutex_lock(&mutDataSpeed);
+		myData.battLevel -= (COEFF_DECHARGE*myData.speed + CONST_DECHARGE);
+		pthread_mutex_unlock(&mutDataSpeed);
+		pthread_mutex_unlock(&mutDataBattLevel);
+		cout << myData.battLevel << endl;
+
+		sleep(1);
 	}
 	return NULL; 
 }	
@@ -83,7 +97,6 @@ void * battery_worker(void * data) {
 void * currPos_worker(void * data) {
 
 	float deltaX, deltaY, dist;
-	cout << "test" << endl;
 
 	while (1) { 
 
@@ -99,7 +112,7 @@ void * currPos_worker(void * data) {
 		myData.currPos.y += deltaY;	
 		pthread_mutex_unlock(&mutDataCurrPos);	
 
-		cout << myData.currPos.x << " " << myData.currPos.y << endl;
+		// cout << "x :" << myData.currPos.x << " y : " << myData.currPos.y << endl;
 	
 		sleep(PERIOD); // period
 	}
@@ -156,11 +169,10 @@ void * display_worker(void * data) {
 void init()
 {
 	myData.speed = 50;
-	myData.angle = 0;
+	myData.angle = 90;
 	myData.battLevel = 100;
 	myData.currPos.x = 0;
 	myData.currPos.y = 0;
-
 
 	pthread_t tid[THREAD_NUM];
 	pthread_attr_t attrib;
@@ -200,54 +212,55 @@ void init()
 	pthread_attr_setinheritsched (&attrib, PTHREAD_EXPLICIT_SCHED);
 	pthread_attr_setschedpolicy (&attrib, SCHED_FIFO);
 
+
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[0], &attrib, cameraControl_worker, NULL ) < 0)
+	if ( pthread_create(&tid[0], NULL, cameraControl_worker, NULL ) < 0)
 		printf("taskSpawn cameraControl_worker failed!\n");
 
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[1], &attrib, camera_worker, NULL ) < 0)
+	if ( pthread_create(&tid[1], NULL, camera_worker, NULL ) < 0)
 		printf("taskSpawn camera_worker failed!\n");
 
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[2], &attrib, battery_worker, NULL ) < 0)
+	if ( pthread_create(&tid[2], NULL, battery_worker, NULL ) < 0)
 		printf("taskSpawn battery_worker failed!\n");
 
 	mySchedParam.sched_priority = 1;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[3], &attrib, currPos_worker, NULL ) < 0)
+	if ( pthread_create(&tid[3], NULL, currPos_worker, NULL ) < 0)
 		cout << "taskSpawn currPos_worker failed!" << endl;
 
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[4], &attrib, angle_worker, NULL ) < 0)
+	if ( pthread_create(&tid[4], NULL, angle_worker, NULL ) < 0)
 		printf("taskSpawn angle_worker failed!\n");
 	
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[5], &attrib, navControl_worker, NULL ) < 0)
+	if ( pthread_create(&tid[5], NULL, navControl_worker, NULL ) < 0)
 		printf("taskSpawn navControl_worker failed!\n");
 
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[6], &attrib, destControl_worker, NULL ) < 0)
+	if ( pthread_create(&tid[6], NULL, destControl_worker, NULL ) < 0)
 		printf("taskSpawn destControl_worker failed!\n");
 
 	mySchedParam.sched_priority = 1;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[7], &attrib, battLow_worker, NULL ) < 0)
+	if ( pthread_create(&tid[7], NULL, battLow_worker, NULL ) < 0)
 		printf("taskSpawn battLow_worker failed!\n");
 
 	mySchedParam.sched_priority = 1;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[8], &attrib, battHigh_worker, NULL ) < 0)
+	if ( pthread_create(&tid[8], NULL, battHigh_worker, NULL ) < 0)
 		printf("taskSpawn battHigh_worker failed!\n");
 
 	mySchedParam.sched_priority = 20;
 	pthread_attr_setschedparam(&attrib, &mySchedParam);
-	if ( pthread_create(&tid[9], &attrib, display_worker, NULL ) < 0)
+	if ( pthread_create(&tid[9], NULL, display_worker, NULL ) < 0)
 		printf("taskSpawn display_worker failed!\n");
 
 	// join des threads
