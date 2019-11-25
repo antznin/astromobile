989108e41859b61d9335b0f87c304af1d4470a02
antznin <antonin.godard@telecom-paristech.fr>
2019-11-25 17:55:26 -0500

    Comment plot_data.py

--- lab03/astromobile/plot_data.py
+++ lab03/astromobile/plot_data.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Lecture des données et stockage dans data
 data_f = open("./data.csv", "r")
 first = data_f.readline().replace("\n", "").split(",")
 data = []
@@ -14,7 +15,8 @@ for line in data_f:
 
 data_f.close()
 
-def unique(seq1, seq2): 
+# Les données d'étapes etc son rendues uniques
+def unique(seq1, seq2):
    checked1 = []
    checked2 = []
    last_el = None
@@ -25,16 +27,12 @@ def unique(seq1, seq2):
            last_el = seq1[i]
    return checked1, checked2
 
-print(data[12][3000])
-print(data[13][3000])
-
-
 for i in range(6,14,2):
     data[i], data[i+1] = unique(data[i], data[i+1])
 
 print(data[12])
 
-# Map plotting
+# Plot de la carte
 plt.figure(1)
 plt.scatter(data[4], data[5],c=data[1], s=4)
 cbar = plt.colorbar()
@@ -42,7 +40,7 @@ cbar.set_label('Niveau de la batterie')
 plt.plot(data[6][:-1], data[7][:-1], 's', c='fuchsia', markersize=4, label="Étapes")
 plt.plot(data[10][1:], data[11][1:], 'go', markersize=4, label="Obstacles")
 plt.plot(data[12][1:], data[13][1:], 'co', markersize=4, label="Stations")
-plt.plot(data[8][:-1], data[9][:-1], 'ro', markersize=5, label="Destinations")
+plt.plot(data[8][:-1], data[9][:-1], 'star', c=yellow, markersize=5, label="Destinations")
 plt.xlabel("$x$")
 plt.ylabel("$y$")
 plt.xticks(fontsize=8)
@@ -51,6 +49,7 @@ plt.legend()
 plt.grid()
 plt.show()
 
+# Plot de la vitesse
 plt.figure(2)
 plt.plot(data[0], data[2])
 plt.grid()
